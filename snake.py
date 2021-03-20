2ef7f165d465948a26e9c1deb6961558b4e7f31d
Yair Yigal <yigalyairn@gmail.com>
2021-03-20 21:02:41 +0200

    cleaned snake code

--- snake.py
+++ snake.py
@@ -1,9 +1,12 @@
 from collections import deque
 
-import numpy as np
 import cv2
+import numpy as np
 
 ACTIONS = ['up', 'right', 'down', 'left']
+
+# Game over causes
+STARVATION, WALL, BODY, FULL = range(4)
 
 VECTORS = [
     np.array([-1, 0]),
@@ -14,38 +17,26 @@ VECTORS = [
 
 
 class Snake:
-    def __init__(self, h, w):
+    def __init__(self, h, w, food_amount=1):
         self.shape = (h, w)
-        self.state_space = (h, w, 2)
-        self.speed_vector = None
-        self.snake = None
-        self.food = None
-        self.action_space = len(ACTIONS)
-        self.max_steps = h * w * 2
-        self.steps = None
-        self.food_amount = None
-        self.frames_queue = deque([], 2)
-        self.game_over = False
-        self.reset()
-
-    def reset(self, food_amount=1):
-        h, w = self.shape
-        self.speed_vector = [0]
+        self.speed_vector = VECTORS[0]
         self.snake = np.array([[h // 2, w // 2]])
         self.food_amount = food_amount
         self.food = None
-        self.food = self._spawn_food()
-        # self.food = np.array([5,5])
+        self._spawn_food()
+        self.max_steps = h * w * 2
         self.steps = 0
-        self.frames_queue.append(self._2d_board())
-        self.frames_queue.append(self._2d_board())
-        return self.get_state()
+        self.game_over = False
+
+        current_board = self._2d_board()
+        self.frames_queue = deque([current_board, current_board], 2)
 
     def step(self, action):
         # no 180 degree turn
         if (VECTORS[action] + self.speed_vector).tolist() != [0, 0]:
             self.speed_vector = VECTORS[action]
 
+        # Move head
         new_head = self.snake[0] + self.speed_vector
 
         h, w = self.shape
@@ -55,38 +46,38 @@ class Snake:
 
         # starvation
         if self.steps > self.max_steps:
-            self.game_over = True
-            return True, 0
+            self.game_over = STARVATION
+            return True
 
         # Collision with walls
         if x < 0 or x >= h or y < 0 or y >= w:
-            self.game_over = True
-            return True, -1
+            self.game_over = WALL
+            return True
 
         # Collision with body
         if new_head.tolist() in self.snake.tolist()[1:]:
-            self.game_over = True
-            return True, -1
+            self.game_over = BODY
+            return True
 
         # Collision with food
         if new_head.tolist() in self.food:
             self.food.remove(new_head.tolist())
             h, w = self.snake.shape
-            self.snake = np.append(new_head, self.snake).reshape((h + 1, w))
             s_h, s_w = self.shape
+            self.snake = np.append(new_head, self.snake).reshape((h + 1, w))
             # snake is all the screen
-            if self.snake.shape[0] - 1 == s_h * s_w:
-                self.game_over = True
+            if h == s_h * s_w:
+                self.game_over = FULL
                 return True
 
-            self.food = self._spawn_food()
+            self._spawn_food()
             self.steps = 0
-            self.frames_queue.append(self._2d_board())
-            return False, 1
 
-        self.snake = np.append(new_head, self.snake)[:-2].reshape(self.snake.shape)
+        else:
+            self.snake = np.append(new_head, self.snake)[:-2].reshape(self.snake.shape)
+
         self.frames_queue.append(self._2d_board())
-        return False, 0
+        return False
 
     @property
     def distance_from_food(self):
@@ -121,36 +112,36 @@ class Snake:
 
         for food in self.food:
             board[tuple(food)] = 0.75
-        # board[tuple(self.snake[0]), :] = (255, 0, 0)
-        # board[tuple(self.food), :] = (0, 255, 0)
         return board
 
-    def get_state(self):
+    @property
+    def last_frames(self):
         return np.dstack(self.frames_queue)
+
+    @property
+    def board(self):
+        return self.frames_queue[-1]
 
     def _spawn_food(self):
         h, w = self.shape
-        foods = 0
-        f = []
-        if self.food is not None:
-            foods = len(self.food)
-            f = self.food
+        if self.food is None:
+            self.food = []
 
-        while foods < self.food_amount:
+        while len(self.food) < self.food_amount:
             x = np.random.randint(0, h)
             y = np.random.randint(0, w)
-            if [x, y] in self.snake.tolist() or [x, y] in f:
+            blocks_left = (h * w) - len(self.food) - self.snake.shape[0]
+            if blocks_left <= 0:
+                return
+
+            if [x, y] in self.snake.tolist() or [x, y] in self.food:
                 continue
 
-            foods += 1
-            f.append([x, y])
-
-        return f
+            self.food.append([x, y])
 
 
 if __name__ == '__main__':
-    s = Snake(6, 6)
-    s.reset(food_amount=1)
+    s = Snake(6, 6, food_amount=5)
     s.max_steps = 1000
     moves = [
         'right',
@@ -195,8 +186,7 @@ if __name__ == '__main__':
     ]
     cv2.namedWindow('snake', cv2.WINDOW_NORMAL)
     for move in moves:
-        end, r= s.step(ACTIONS.index(move))
-        cv2.imshow('snake', s.frames_queue[-1])
+        end = s.step(ACTIONS.index(move))
+        cv2.imshow('snake', s.board)
         cv2.waitKey(100)
-        print(move)
-        print(f'r={r}, end={end}')
+        print(f'move={move}, end={end}')
